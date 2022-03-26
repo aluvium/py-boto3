7c947eb6417ff60e3846065170cc83b939754570
aluvium <aluvium@protonmail.com>
2022-03-26 09:11:40 +0100

    List of all aws users from iam.

--- user-list.py
+++ user-list.py
@@ -1,9 +1,11 @@
 import boto3
 
-AWS_M_CONF=boto3.session.Session(profile_name="root")
-IAM_CONF=AWS_M_CONF.resource('iam')
+def usr_list(name):
+    AWS_M_CONF=boto3.session.Session(profile_name="root")
+    IAM_CONF=AWS_M_CONF.resource(name)
+    for each_u in IAM_CONF.users.all():
+        print(each_u.name)
+    return 0
 
-for each_u in IAM_CONF.users.all():
-    print(each_u)
-
+usr_list('iam')
 
