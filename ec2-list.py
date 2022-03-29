107665afcf4c248669b4585a57b784fb4348576a
aluvium <aluvium@protonmail.com>
2022-03-29 12:32:54 +0200

    List all EC2 instances and thier  Public IP Addresses - In the block there are possiblity to use other monitoring keys such as -> ImageID, InstanceType, KeyName, LaunchTime, Monitoring, Platform, PublicDnsName, PrivateIpAddress, BlockDeviceMappings, Hypervisor, Monitoring, NetworkInterfaces, and ohters..

---
+++ ec2-list.py
@@ -0,0 +1,17 @@
+import boto3
+
+def ec2_list_cl(alice, betty):
+    AWS_M_CON=boto3.session.Session(profile_name='root')
+    EC2_CON=AWS_M_CON.client('ec2','eu-central-1')
+    response=EC2_CON.describe_instances()['Reservations']
+    for each_group in response:
+        for each_inst in each_group['Instances']:
+            print (each_inst[alice], each_inst[betty])
+    return 0
+
+# Other usefull keys for monitorinig ==> 
+# PrivateIpAddress, LaunchTime, ImageId, InstanceType, KeyName, BlockDeviceMapping, NetworkInterfaces
+# for all --> print (each_inst) without key
+
+ec2_list_cl('InstanceId','PublicIpAddress')
+
