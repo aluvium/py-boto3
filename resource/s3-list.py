e92e2feebd7f35b1f4a3bb9310afd4833f49e7aa
aluvium <aluvium@protonmail.com>
2022-03-30 16:51:43 +0200

    small changes

--- resource/s3-list.py
+++ resource/s3-list.py
@@ -1,9 +1,9 @@
 import boto3
 
 def s3_list(name):
-    AWS_M_CONF=boto3.session.Session(profile_name="root")
-    IAM_CONF=AWS_M_CONF.resource(name)
-    for each_b in IAM_CONF.buckets.all():
+    AWS_M_CON=boto3.session.Session(profile_name="root")
+    IAM_CON=AWS_M_CON.resource(name)
+    for each_b in IAM_CON.buckets.all():
         print(each_b.name)
     return 0
 
--- resource/user-list.py
+++ resource/user-list.py
@@ -1,9 +1,9 @@
 import boto3
 
 def usr_list(name):
-    AWS_M_CONF=boto3.session.Session(profile_name="root")
-    IAM_CONF=AWS_M_CONF.resource(name)
-    for each_u in IAM_CONF.users.all():
+    AWS_M_CON=boto3.session.Session(profile_name="root")
+    IAM_CON=AWS_M_CON.resource(name)
+    for each_u in IAM_CON.users.all():
         print(each_u.name)
     return 0
 
