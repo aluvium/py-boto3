899f06d01088e6d8313eaa0bb11eaf53f02645ef
aluvium <aluvium@protonmail.com>
2022-03-30 17:30:51 +0200

    Console for EC2 Instances Collections Group List of working instaces, state, ip and more For limit amount of instances expect all() use limit(some-number)

---
+++ resource/ec2-console.py
@@ -0,0 +1,15 @@
+#working with multi-instances
+import boto3
+
+def ec2_list():
+    AWS_M_CON=boto3.session.Session(profile_name="root")
+    EC2_CON=AWS_M_CON.resource('ec2', 'eu-central-1')
+    for each in EC2_CON.instances.all():
+# all possible options  ----> vv <----
+#       for e in dir(each):
+#            print(e)
+        print(each.id, each.state, each.public_ip_address)
+    return 0
+
+ec2_list()
+
