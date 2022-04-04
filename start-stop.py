5a2ecb1cdb4dfbcdf81023718b58eda22d829a87
aluvium <aluvium@protonmail.com>
2022-04-04 21:11:28 +0200

    start | stop | terminate EC2 instance using id

--- start-stop.py
+++ start-stop.py
@@ -3,16 +3,9 @@ import sys
 from os import system
 
 AWS_M_CON=boto3.session.Session(profile_name="root")
-EC2_RE=AWS_M_CON.resource("ec2", "eu-cental-1")
+EC2_RE=AWS_M_CON.resource("ec2", "eu-central-1")
 EC2_CLI=AWS_M_CON.client("ec2", "eu-central-1")
 
-def chosen_id():
-    inst_id=input('Please specify EC2 IP address: ')
-    chosen_inst=EC2_RE.Instance(inst_id)
-    system('clear')
-    print(dir(chosen_inst))
-    chosen_inst.start()
-    return 0
 
 while True:
     print("""
@@ -29,16 +22,25 @@ PRESS:  - - - - - - - - - - -
     """)
     opt=int(input("Enter the number: "))
     if opt==1:
-        chosen_id()
+        inst_id=input('Please specify EC2 id: ')
+        chosen_inst=EC2_RE.Instance(inst_id)
+        system('clear')
         print("Starting ec2 instance...")
+        chosen_inst.start()
 
     elif opt==2:
-        chosen_id()  
-        print("Stoping ec2 instance...")
+        inst_id=input('Please specify EC2 id: ')
+        chosen_inst=EC2_RE.Instance(inst_id)
+        system('clear')
+        print("Stopping ec2 instance...")
+        chosen_inst.stop()
 
     elif opt==3:
-        chosen_id()
+        inst_id=input('Please specify EC2 id: ')
+        chosen_inst=EC2_RE.Instance(inst_id)
+        system('clear')
         print("Terminating ec2 instance...")
+        chosen_inst.terminate()
 
     elif opt==4:
         system('clear')
