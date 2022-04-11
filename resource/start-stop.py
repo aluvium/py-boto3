3b79fe5867038a5afcc7e854ad661ec8a02197bd
aluvium <aluvium@protonmail.com>
2022-04-11 13:04:26 +0200

    def functions

--- resource/start-stop.py
+++ resource/start-stop.py
@@ -5,8 +5,15 @@ from os import system
 AWS_M_CON=boto3.session.Session(profile_name="root")
 EC2_RE=AWS_M_CON.resource("ec2", "eu-central-1")
 
-while True:
-    print("""
+def input_1():
+    inst_id=input('Please specify EC2 id: ')
+    value=EC2_RE.Instance(inst_id)
+    system('clear')
+    return value
+
+def main():
+    while True:
+        print("""
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 PRESS:  - - - - - - - - - - - 
@@ -18,35 +25,29 @@ PRESS:  - - - - - - - - - - -
 
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     """)
-    opt=int(input("Enter the number: "))
-    if opt==1:
-        inst_id=input('Please specify EC2 id: ')
-        chosen_inst=EC2_RE.Instance(inst_id)
-        system('clear')
-        print("Starting ec2 instance...")
-        chosen_inst.start()
-        chosen_inst.wait_until_running()  
-    elif opt==2:
-        inst_id=input('Please specify EC2 id: ')
-        chosen_inst=EC2_RE.Instance(inst_id)
-        system('clear')
-        print("Stopping ec2 instance...")
-        chosen_inst.stop()
-        chosen_inst.wait_until_stopped()
+        opt=int(input("Enter the number: "))
+        if opt==1:
+            chosen_inst=input_1()
+            print("Starting ec2 instance...")
+            chosen_inst.start()
+            chosen_inst.wait_until_running()  
+        elif opt==2:
+            chosen_inst=input_1()
+            print("Stopping ec2 instance...")
+            chosen_inst.stop()
+            chosen_inst.wait_until_stopped()
+        elif opt==3:
+            chosen_inst=EC2_RE.Instance(inst_id)
+            print("Terminating ec2 instance...")
+            chosen_inst.terminate()
+            chosen_inst.wait_until_terminated()
+        elif opt==4:
+            system('clear')
+            print("Goodbye")
+            sys.exit()
+        else:
+            print("Your input is invalid. Try again.")
+    return None
+main()
 
-    elif opt==3:
-        inst_id=input('Please specify EC2 id: ')
-        chosen_inst=EC2_RE.Instance(inst_id)
-        system('clear')
-        print("Terminating ec2 instance...")
-        chosen_inst.terminate()
-        chosen_inst.wait_until_terminated()
-        
 
-    elif opt==4:
-        system('clear')
-        print("Goodbye")
-        sys.exit()
-
-    else:
-        print("Your input is invalid. Try again.")
